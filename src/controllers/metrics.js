b02af13cf79b71e33f643956d93752842f142604
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-17 19:42:48 +0530

    feat: integrate src/controllers/metrics.js

---
+++ src/controllers/metrics.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const metricsService = require('../services/metrics');
+
+router.get('/', async (req, res) => {
+    const metrics = await metricsService.getMetrics();
+    res.json(metrics);
+});
+
+module.exports = router;
